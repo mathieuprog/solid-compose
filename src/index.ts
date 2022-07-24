0d51cde2cd42667e4328add6460043b433132120
mathieuprog <5883963+mathieuprog@users.noreply.github.com>
2022-07-24 05:43:32 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import ColorSchemeStylesheet from "./ColorSchemeStylesheet";
+import getSystemColorScheme from "./getSystemColorScheme";
+import useColorScheme, { ColorSchemeProvider, ColorSchemeStorage } from "./useColorScheme";
+import useLocalStorage from "./useLocalStorage";
+
+export {
+  ColorSchemeProvider,
+  ColorSchemeStorage,
+  ColorSchemeStylesheet,
+  getSystemColorScheme,
+  useColorScheme,
+  useLocalStorage,
+}
---
+++ src/useLocalStorage.test.ts
@@ -0,0 +1,46 @@
+import { createRoot } from "solid-js";
+import useLocalStorage from './useLocalStorage';
+
+describe("useLocalStorage", () => {
+  test("initialize state with pre-existing value from localStorage", () => {
+    createRoot((dispose) => {
+      localStorage.setItem('foo', JSON.stringify('hello'));
+
+      const [value, { remove: removeValue }] = useLocalStorage('foo', 'world');
+
+      expect(value()).toEqual('hello');
+
+      removeValue();
+
+      expect(value()).toBeNull();
+      expect(localStorage.getItem('foo')).toBeNull();
+
+      dispose();
+    });
+  });
+
+  test("update state and localStorage", () => {
+    createRoot((dispose) => {
+      expect(localStorage.getItem('foo')).toBeNull();
+
+      const [value, { set: setValue, remove: removeValue }] = useLocalStorage('foo', 'world');
+      useLocalStorage('bar');
+
+      expect(JSON.parse(localStorage.getItem('foo') as string)).toEqual('world');
+
+      expect(value()).toEqual('world');
+
+      setValue('hello');
+
+      expect(value()).toEqual('hello');
+      expect(JSON.parse(localStorage.getItem('foo') as string)).toEqual('hello');
+
+      removeValue();
+
+      expect(value()).toBeNull();
+      expect(localStorage.getItem('foo')).toBeNull();
+
+      dispose();
+    });
+  });
+});
