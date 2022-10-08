9c066303dfc9b0878ff4a9a88620443cba0eb225
mathieuprog <5883963+mathieuprog@users.noreply.github.com>
2022-10-08 10:56:35 +0200

    Allow to pass default value to storage and add new signal storage

--- src/theme/useColorScheme.tsx
+++ src/theme/useColorScheme.tsx
@@ -1,5 +1,10 @@
 import type { Accessor, ParentComponent } from 'solid-js';
-import { createContext, mergeProps, useContext } from 'solid-js';
+import {
+  createContext,
+  createSignal,
+  mergeProps,
+  useContext
+} from 'solid-js';
 import getSystemColorScheme from './getSystemColorScheme';
 import useLocalStorage from '../storage/useLocalStorage';
 
@@ -12,9 +17,10 @@ export interface ColorSchemeStore {
 
 interface Props {
   storage?: ColorSchemeStorage;
+  defaultScheme?: ColorScheme;
 }
 
-export type ColorSchemeStorage = () => [Accessor<ColorScheme | null>, (colorScheme: ColorScheme) => void];
+export type ColorSchemeStorage = (defaultValue?: ColorScheme) => [Accessor<ColorScheme | null>, (colorScheme: ColorScheme) => void];
 
 type ColorSchemeStorageDict = {
   [key: string]: ColorSchemeStorage;
@@ -30,9 +36,12 @@ export const ColorSchemeStorage: ColorSchemeStorageDict = {
   mediaQuery: () => {
     return [getSystemColorScheme, () => { throw new Error() }];
   },
-  localStorage: () => {
-    const [colorScheme, { set: setColorScheme }] = useLocalStorage<ColorScheme>('__color-scheme', getSystemColorScheme());
+  localStorage: (defaultValue) => {
+    const [colorScheme, { set: setColorScheme }] = useLocalStorage<ColorScheme>('__color-scheme', defaultValue || getSystemColorScheme());
     return [colorScheme, setColorScheme];
+  },
+  signalStorage: (defaultValue) => {
+    return createSignal(defaultValue || getSystemColorScheme());
   },
   queryString: () => {
     const get = () => {
@@ -58,7 +67,7 @@ const ColorSchemeContext = createContext<ColorSchemeContext>(defaultValue);
 export const ColorSchemeProvider: ParentComponent<Props> = (props) => {
   const mergedProps = mergeProps({ storage: ColorSchemeStorage.mediaQuery }, props);
 
-  const [colorScheme, setColorScheme] = mergedProps.storage();
+  const [colorScheme, setColorScheme] = mergedProps.storage(mergedProps.defaultScheme);
 
   const setColorScheme_: ColorSchemeSetter = (arg) => {
     if(typeof arg === 'function') {
