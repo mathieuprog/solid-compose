4774ffaedc4757626bc8368b4e7799526b8b6313
mathieuprog <5883963+mathieuprog@users.noreply.github.com>
2022-10-15 06:08:38 +0200

    Expose i18n functions

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
-import ColorSchemeStylesheet from "./theme/ColorSchemeStylesheet";
-import getSystemColorScheme from "./theme/getSystemColorScheme";
-import useColorScheme, { ColorSchemeProvider, ColorSchemeStorage } from "./theme/useColorScheme";
-import useLocalStorage from "./storage/useLocalStorage";
+import ColorSchemeStylesheet from './theme/ColorSchemeStylesheet';
+import getSystemColorScheme from './theme/getSystemColorScheme';
+import useColorScheme, { ColorSchemeProvider, ColorSchemeStorage } from './theme/useColorScheme';
+import useLocalStorage from './storage/useLocalStorage';
+import use18n, { I18nProvider, addTranslations } from './i18n/useI18n';
 
 export {
+  addTranslations,
+  I18nProvider,
+  use18n,
+
   ColorSchemeProvider,
   ColorSchemeStorage,
   ColorSchemeStylesheet,
   getSystemColorScheme,
   useColorScheme,
-  useLocalStorage,
+
+  useLocalStorage
 }
