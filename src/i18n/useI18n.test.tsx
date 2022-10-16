e8f4f9f131d53c52f329325df1e3f5bad92b8c01
mathieuprog <5883963+mathieuprog@users.noreply.github.com>
2022-10-16 09:40:57 +0200

    Add support for JSON files

--- src/i18n/useI18n.test.tsx
+++ src/i18n/useI18n.test.tsx
@@ -6,88 +6,91 @@ import use18n, {
   I18nProvider,
   removeAllTranslations
 } from './useI18n';
+import enTranslations from './support/en.json';
+import frTranslations from './support/fr.json';
 
-describe("useI18n", () => {
+describe('useI18n', () => {
   beforeEach(() => {
     removeAllTranslations();
     enableNestedTranslations(false);
 
-    addTranslations("en", "foo", {
+    addTranslations('en', 'foo', {
       "hello": "hello",
       "foo": "bar"
     });
 
-    addTranslations("fr", "foo", {
+    addTranslations('fr', 'foo', {
       "hello": "bonjour",
     });
 
-    addTranslations("en", "bar", {
+    addTranslations('en', 'bar', {
       "hello": "hello!",
       "world": "world!"
     });
 
-    addTranslations("fr", "bar", {
+    addTranslations('fr', 'bar', {
       "hello": "bonjour !",
       "world": "monde !"
     });
 
-    addTranslations("fr-BE", "bar", {
+    addTranslations('fr-BE', 'bar', {
       "world": "monde !!"
     });
   });
 
   afterEach(cleanup);
 
-  test("translate", async () => {
+  test('translate', async () => {
     function Hello() {
       const [translate, locale] = use18n();
       return <>
         <div data-testid="hello">{translate('hello')}</div>
         <div data-testid="world">{translate('world')}</div>
         <div data-testid="foo">{translate('foo')}</div>
-        <button data-testid="locale" onClick={() => locale("fr-BE")}>
+        <button data-testid="locale" onClick={() => locale('fr-BE')}>
           {locale()}
         </button>
       </>;
     }
 
     render(() =>
-      <I18nProvider locale="en" namespaces={["foo", "bar"]}>
+      <I18nProvider locale="en" namespaces={['foo', 'bar']}>
         <Hello/>
       </I18nProvider>
     );
 
-    const hello = screen.getByTestId("hello");
-    const world = screen.getByTestId("world");
-    const foo = screen.getByTestId("foo");
-    const locale = screen.getByTestId("locale");
+    const hello = screen.getByTestId('hello');
+    const world = screen.getByTestId('world');
+    const foo = screen.getByTestId('foo');
+    const locale = screen.getByTestId('locale');
 
-    expect(hello.textContent).toBe("hello!");
-    expect(world.textContent).toBe("world!");
-    expect(foo.textContent).toBe("bar");
-    expect(locale.textContent).toBe("en");
+    expect(hello.textContent).toBe('hello!');
+    expect(world.textContent).toBe('world!');
+    expect(foo.textContent).toBe('bar');
+    expect(locale.textContent).toBe('en');
 
     fireEvent.click(locale);
     // the event loop takes one Promise to resolve to be finished
     await Promise.resolve();
 
-    expect(locale.textContent).toBe("fr-BE");
-    expect(hello.textContent).toBe("bonjour !");
-    expect(world.textContent).toBe("monde !!");
-    expect(foo.textContent).toBe("bar");
+    expect(locale.textContent).toBe('fr-BE');
+    expect(hello.textContent).toBe('bonjour !');
+    expect(world.textContent).toBe('monde !!');
+    expect(foo.textContent).toBe('bar');
   });
 
-  test("key separator", async () => {
+  test('key separator', async () => {
+    removeAllTranslations();
     enableNestedTranslations('.');
 
-    addTranslations("en", "foo", {
+    addTranslations('en', 'foo', {
       "welcome": {
         "hello": "hello!"
       },
       "world": "world!"
     });
 
-    addTranslations("fr", "foo", {
+    addTranslations('fr', 'foo', {
       "welcome": {
         "hello": "bonjour !"
       },
@@ -99,41 +102,43 @@ describe("useI18n", () => {
       return <>
         <div data-testid="hello">{translate('welcome.hello')}</div>
         <div data-testid="world">{translate('world')}</div>
-        <button data-testid="locale" onClick={() => locale("fr-BE")}>
+        <button data-testid="locale" onClick={() => locale('fr-BE')}>
           {locale()}
         </button>
       </>;
     }
 
     render(() =>
-      <I18nProvider locale="en" namespaces={["foo"]}>
+      <I18nProvider locale="en" namespaces={['foo']}>
         <Hello/>
       </I18nProvider>
     );
 
-    const hello = screen.getByTestId("hello");
-    const world = screen.getByTestId("world");
-    const locale = screen.getByTestId("locale");
+    const hello = screen.getByTestId('hello');
+    const world = screen.getByTestId('world');
+    const locale = screen.getByTestId('locale');
 
-    expect(hello.textContent).toBe("hello!");
-    expect(world.textContent).toBe("world!");
+    expect(hello.textContent).toBe('hello!');
+    expect(world.textContent).toBe('world!');
 
     fireEvent.click(locale);
     // the event loop takes one Promise to resolve to be finished
     await Promise.resolve();
 
-    expect(locale.textContent).toBe("fr-BE");
-    expect(hello.textContent).toBe("bonjour !");
-    expect(world.textContent).toBe("monde !");
+    expect(locale.textContent).toBe('fr-BE');
+    expect(hello.textContent).toBe('bonjour !');
+    expect(world.textContent).toBe('monde !');
   });
 
-  test("default namespace", async () => {
-    addTranslations("en", {
+  test('default namespace', async () => {
+    removeAllTranslations();
+
+    addTranslations('en', {
       "hello": "hello!",
       "world": "world!"
     });
 
-    addTranslations("fr", {
+    addTranslations('fr', {
       "hello": "bonjour !",
       "world": "monde !"
     });
@@ -142,7 +147,7 @@ describe("useI18n", () => {
       const [translate, locale] = use18n();
       return <>
         <div data-testid="hello">{translate('hello')}</div>
-        <button data-testid="locale" onClick={() => locale("fr-BE")}>
+        <button data-testid="locale" onClick={() => locale('fr-BE')}>
           {locale()}
         </button>
       </>;
@@ -154,16 +159,51 @@ describe("useI18n", () => {
       </I18nProvider>
     );
 
-    const hello = screen.getByTestId("hello");
-    const locale = screen.getByTestId("locale");
+    const hello = screen.getByTestId('hello');
+    const locale = screen.getByTestId('locale');
 
-    expect(hello.textContent).toBe("hello!");
+    expect(hello.textContent).toBe('hello!');
 
     fireEvent.click(locale);
     // the event loop takes one Promise to resolve to be finished
     await Promise.resolve();
 
-    expect(locale.textContent).toBe("fr-BE");
-    expect(hello.textContent).toBe("bonjour !");
+    expect(locale.textContent).toBe('fr-BE');
+    expect(hello.textContent).toBe('bonjour !');
+  });
+
+  test('add translations from json files', async () => {
+    removeAllTranslations();
+
+    addTranslations('en', enTranslations);
+    addTranslations('fr', frTranslations);
+
+    function Hello() {
+      const [translate, locale] = use18n();
+      return <>
+        <div data-testid="hello">{translate('hello')}</div>
+        <button data-testid="locale" onClick={() => locale('fr-BE')}>
+          {locale()}
+        </button>
+      </>;
+    }
+
+    render(() =>
+      <I18nProvider locale="en">
+        <Hello/>
+      </I18nProvider>
+    );
+
+    const hello = screen.getByTestId('hello');
+    const locale = screen.getByTestId('locale');
+
+    expect(hello.textContent).toBe('hello!');
+
+    fireEvent.click(locale);
+    // the event loop takes one Promise to resolve to be finished
+    await Promise.resolve();
+
+    expect(locale.textContent).toBe('fr-BE');
+    expect(hello.textContent).toBe('bonjour !');
   });
 });
