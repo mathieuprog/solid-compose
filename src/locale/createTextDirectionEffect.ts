850133b2aac2da0126ce48c566d7546ee82ec703
mathieuprog <5883963+mathieuprog@users.noreply.github.com>
2023-04-12 16:50:11 +0200

    Allow createTextDirectionEffect outside of components

--- src/locale/createTextDirectionEffect.ts
+++ src/locale/createTextDirectionEffect.ts
@@ -1,19 +1,21 @@
-import { createEffect } from 'solid-js';
+import { createEffect, createRoot } from 'solid-js';
 import { useLocale } from '..';
 import TextDirection from './TextDirection';
 
 export default function createTextDirectionEffect() {
   const [locale] = useLocale();
 
-  createEffect(() => {
-    switch (locale.textDirection) {
-      case TextDirection.LeftToRight:
-        document.documentElement.dir = 'ltr';
-        break;
+  createRoot(() => {
+    createEffect(() => {
+      switch (locale.textDirection) {
+        case TextDirection.LeftToRight:
+          document.documentElement.dir = 'ltr';
+          break;
 
-      case TextDirection.RightToLeft:
-        document.documentElement.dir = 'rtl';
-        break;
-    }
+        case TextDirection.RightToLeft:
+          document.documentElement.dir = 'rtl';
+          break;
+      }
+    });
   });
 };
