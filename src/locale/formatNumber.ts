2220c2df1f6dfa82086056b6764a5d6d1ad85296
mathieuprog <5883963+mathieuprog@users.noreply.github.com>
2023-09-02 05:17:17 +0200

    Refactor formatNumber

--- src/locale/formatNumber.ts
+++ src/locale/formatNumber.ts
@@ -1,17 +1,8 @@
-import { NumberFormat } from 'user-locale';
+import { numberFormatter } from 'user-locale';
 import useLocale from './useLocale';
 
 export default function formatNumber(number: number, options: { [key: string]: unknown } = {}) {
   const [locale] = useLocale();
 
-  switch (locale.numberFormat) {
-    case NumberFormat.CommaPeriod:
-      return Intl.NumberFormat('en-US', options).format(number);
-
-    case NumberFormat.PeriodComma:
-      return Intl.NumberFormat('de-DE', options).format(number);
-
-    case NumberFormat.SpaceComma:
-      return Intl.NumberFormat('fr-FR', options).format(number);
-  }
+  return numberFormatter(locale.numberFormat)(number, options);
 }
