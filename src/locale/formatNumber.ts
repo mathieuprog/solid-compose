464b2ed09df06e2043702520093c99c1f80aa271
mathieuprog <5883963+mathieuprog@users.noreply.github.com>
2023-10-11 11:15:22 +0200

    Update formatNumber types

--- src/locale/formatNumber.ts
+++ src/locale/formatNumber.ts
@@ -1,7 +1,7 @@
 import { numberFormatter } from 'user-locale';
 import useLocale from './useLocale';
 
-export default function formatNumber(number: number, options: { [key: string]: unknown } = {}) {
+export default function formatNumber(number: number | string, options: { [key: string]: unknown } = {}) {
   const [locale] = useLocale();
 
   return numberFormatter(locale.numberFormat)(number, options);
